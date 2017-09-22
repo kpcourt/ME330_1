496f2991caf0e4829caaa44d4d4278d565203381
kpcourt <32206481+kpcourt@users.noreply.github.com>
2017-09-22 14:31:51 -0500

    Add files via upload

---
+++ offset_find.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 19:49:51 2017
+
+@author: Kevin
+"""
+
+E = 180294 #Modulous find using excel make sure units match xlist
+
+ylist = []
+xlist = []
+Data_File = open('Data.txt','r') 
+#Data.txt is text file with two coloums of numbers stain,stress no lables 
+#make sure stress and modulus units match strain is in mm/mm
+#also make sure Data.txt is in the same directory as this file
+for line in Data_File:
+    xlist.append(float(line.split()[0]))
+    ylist.append(float(line.split()[1]))
+Data_File.close()
+
+for i in range(len(xlist)):
+    try:
+        x1 = xlist[i]
+        x2 = xlist[i+1]
+        y1 = ylist[i]
+        y2 = ylist[i+1]
+    except:
+        pass
+    if (x2-x1)!= 0:
+        m = (y2-y1)/(x2-x1)
+    else:
+        i+=1
+    if m != E:
+        y = ((-m*0.002)+(y2-m*x2))*(1/(1-(m/E)))
+    else:
+        i+=1        
+    if y>=y1 and y<=y2:
+        print("Offset= "+str(y))
+    else:
+        i+=1
+
